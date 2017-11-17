762fdad079b1d9b544f39bf2a9328ca6b7ddfc8b
vkyho96 <vkyho96@gmail.com>
2017-11-17 13:37:32 -0800

    show case update

--- bs.js
+++ bs.js
@@ -23,11 +23,53 @@
 
 $(function(){
   $(window).scroll(function() {
+
+
+
+    // nav fade in on scroll
      if($(window).scrollTop() >= 100) {
        $('nav').addClass('scrolled');
+       
+
      }
     else {
       $('nav').removeClass('scrolled');
+
+    }
+  });
+});
+
+//zoom on scrollLink
+
+
+
+// active link switching
+
+$(window).scroll(function(){
+
+
+  var scrollbarLocation = $(this).scrollTop();
+  //console.log(scrollbarLocation);
+
+  //for each scrollLink
+  scrollLink.each(function(){
+
+    //error on top :(
+    // var sectionOffset = 0;$(this.hash).offset().top;
+
+    // relace these lines with top error
+    var sectionOffset = 0;
+    target = $(this.hash);
+
+    if (target.length){
+     sectionOffset = target.offset().top;
+    }﻿
+    // end of top error replacement
+
+
+    if (sectionOffset <= scrollbarLocation) {
+      $(this).parent().addClass('active');
+      $(this).parent().siblings().removeClass('active');
     }
   });
 });
